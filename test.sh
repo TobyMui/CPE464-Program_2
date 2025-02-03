0f864a0ab2ed9c25b2bb822b4a7024f87c3765ef
TobyMui <tcmui@calpoly.edu>
2025-02-03 10:41:11 -0800

    done?

--- test.sh
+++ test.sh
@@ -2,17 +2,18 @@
 
 # Server info
 host="localhost"
-port=12345  # Replace with your desired port
+port=55207  # Replace with your desired port
 
 # Create a new tmux session
 tmux new-session -d -s client_session
 
 # Loop through 500 clients
-for i in {1..500}
+for i in {1..1000}
 do
     # Create a new tmux window for each client and run the client command
-    tmux new-window -t client_session:handle$i "./cclient handle$i $host $port $i"
+    tmux new-window -t client_session: -n "handle$i" "./cclient handle$i $host $port"
     echo "Started client with handle$i in tmux window handle$i"
+
 done
 
 # Attach to the tmux session to view the windows
